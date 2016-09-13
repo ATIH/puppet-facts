c1921e6f30c746a58f56d887ffb354dcb928b0e0
bsauvajon <brice.sauvajon@atih.sante.fr>
2016-09-13 13:26:52 +0200

    correction facterpath

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class facts::params {
 
   case $::operatingsystem {
     'windows': {
-      $facterpath = '${::windows_common_appdata}\PuppetLabs\facter\facts.d'
+      $facterpath = "${::windows_common_appdata}\PuppetLabs\facter\facts.d"
     }
     default: {
       $facterpath = '/etc/facter/facts.d'
